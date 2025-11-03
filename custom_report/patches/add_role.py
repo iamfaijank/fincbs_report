c02c336ace0f2bb2b654b97be93216f8ecd48876
imatul1996 <atulgnadekar@gmail.com>
2025-11-03 13:32:48 +0530

    feat: add patches for role

---
+++ custom_report/patches/add_role.py
@@ -0,0 +1,23 @@
+import frappe
+ 
+def execute():
+    roles = [
+        "HR Department Report",
+        "JLL Department Report",
+        "MIS Department Report",
+        "Loan Department Report",
+        "Audit Department Report",
+        "Finance Department Report",
+        "Operation Department Report",
+        "Two Wheeler Department Report",
+        "Branch Report"
+    ]
+ 
+    for role in roles:
+        if not frappe.db.exists("Role", role):
+            frappe.get_doc({
+                "doctype": "Role",
+                "role_name": role,
+                "desk_access": 1
+            }).insert(ignore_permissions=True)
+ 
